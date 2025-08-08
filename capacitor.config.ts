655f33f299cc61f8032caeb1d59e03c0fe6e1925
BAD-WOLF <matheusviaira160@gmail.com>
2025-08-08 12:50:28 -0300

    feat: 🎸 add Capacitor support with Android platform integration
    - Added `capacitor.config.ts` with app metadata and build output settings
    - Added Android platform folder (`android/..`)
    - Installed Capacitor packages:
      - @capacitor/android
      - @capacitor/cli
      - @capacitor/core
    - Updated `vite.config.ts` to allow all hosts
    - Updated `package.json` and `yarn.lock` to include new dependencies

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vieira.tiktokshop',
+  appName: 'TikTok Shop',
+  webDir: 'dist'
+};
+
+export default config;
